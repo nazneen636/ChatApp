aefe81ef869b0804d7804e5da9a5329e94185a64
nazneen636 <nazneennahar636@gmail.com>
2025-06-08 22:33:11 +0600

    bug fix

--- src/pages/Error/NotSigned.jsx
+++ src/pages/Error/NotSigned.jsx
@@ -13,12 +13,12 @@ const NotSignedIn = () => {
         </p>
         <div className="mt-10 flex items-center justify-center gap-x-6">
           <a
-            href="#"
+            href="/signin"
             className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
           >
             Go to Sign in
           </a>
-          <Link to="/signin" className="text-sm font-semibold text-gray-900">
+          <Link to="#" className="text-sm font-semibold text-gray-900">
             Contact support <span aria-hidden="true">&rarr;</span>
           </Link>
         </div>
